1dc47492da52a715aa380ab6db9ab141610c067d
Eumel <Eumel@wlan-141-23-102-48.tubit.tu-berlin.de>
2014-01-23 14:54:09 +0100

    working version of vizualisation of grpah with d3 and quering of neo4j db

---
+++ js/graph_viz.js
@@ -0,0 +1,142 @@
+function draw_graph(nodes_for_viz,relations_for_viz) {
+
+var width = 960,
+    height = 700;
+
+
+var force = d3.layout.force()
+    .nodes(nodes_for_viz)
+    .links(relations_for_viz)
+    .size([width, height])
+    .linkDistance(140)
+    .charge(-1000)
+    .on("tick", tick)
+    .start();
+
+var fisheye = d3.fisheye.circular()
+    .radius(200)
+    .distortion(2);
+
+// remove old graph
+$('#graph_svg').remove();
+
+// make new graphic
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("id", 'graph_svg');
+
+
+
+var link = svg.selectAll(".link")
+    .data(force.links())
+    .enter().append("line")
+    .attr("class", "link")
+    .style("stroke-width", function(d) { if (d.type == "IN_STATE") {return 1;} else{return 4;} })
+    .on("mouseover", link_mouseover)
+    .on("mouseout", link_mouseout);
+
+link.append("text")
+    .attr("x", 12)
+    .attr("dy", ".35em")
+    .style("fill", "#000")
+    .text(function (d) {
+        return d.pmi;
+    });
+// 
+var node = svg.selectAll(".node")
+    .data(force.nodes())
+    .enter().append("g")
+    .attr("class", "node")
+    .style("fill", function(d) { if (d.type == "stat") {return "blue";} else{return "red";} })
+    .on("mouseover", node_mouseover)
+    .on("mouseout", node_mouseout)
+    .call(force.drag);
+// 
+node.append("circle")
+    .attr("r", 8);
+// 
+node.append("text")
+    .attr("x", 12)
+    .attr("dy", ".35em")
+    .style("fill", "#000")
+    .text(function (d) {
+        return d.text;
+    });
+
+function tick() {
+    link
+        .attr("x1", function (d) {
+            return d.source.x;
+        })
+        .attr("y1", function (d) {
+            return d.source.y;
+        })
+        .attr("x2", function (d) {
+            return d.target.x;
+        })
+        .attr("y2", function (d) {
+            return d.target.y;
+        });
+
+    node
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+}
+}
+
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
+
+d3.selection.prototype.moveToBack = function() { 
+    return this.each(function() { 
+        var firstChild = this.parentNode.firstChild; 
+        if (firstChild) { 
+            this.parentNode.insertBefore(this, firstChild); 
+        } 
+    }); 
+};
+
+
+function node_mouseover() {
+    d3.select(this).select("circle").transition()
+        .duration(750)
+        .attr("r", 16);
+   d3.select(this).select("text")
+        .transition()
+        .duration(750)
+	    .attr("x", 20)
+	    .attr("dy", ".70em")
+		.attr("class", "shadow")
+        .style("font-size","20px");
+		d3.select(this).moveToFront();
+}
+
+function node_mouseout() {
+    d3.select(this).select("circle").transition()
+        .duration(750)
+        .attr("r", 8);
+   d3.select(this).select("text").transition()
+       .duration(750)
+       .attr("x", 12)
+       .attr("dy", ".35em")
+       .style("font-size","10px");
+		// d3.select(this).moveToBack();
+	console.log(d3.select(this));
+
+}
+
+function link_mouseover() {
+	console.log("over link")
+    d3.select(this).select("line").transition()
+        .style("stroke-width", 8);
+}
+
+function link_mouseout() {
+    d3.select(this).select("line")//.transition()
+        .style("stroke-width", 2);
+}
---
+++ js/query_neo4j.js
@@ -0,0 +1,105 @@
+function query_db(url, query) {
+    var data = {
+        "query": query,
+        // "params" : params.
+    }
+
+    jQuery.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        dataType: "json",
+        success: function (data) {
+            parse_results(data)
+        }, //,
+        error: function (data) {
+            console.log('error');
+        }
+    });
+}
+// get id from a neo4j url  
+function id_from_url(url) {
+    return url.split('/').slice(-1)[0];
+}
+
+function parse_results(data) {
+
+    var nodes_from_query = [];
+    var relations_for_viz = [];
+    var results = data.data;
+    // console.log(data);
+    // console.log(results);
+
+    for (var i = 0; i < results.length; i++) {
+
+        // add noun nodes 
+        for (var noun_ind = 0; noun_ind < results[i][0].length; noun_ind++) {
+            var noun_data = results[i][0][noun_ind];
+            nodes_from_query.push({
+                'index': noun_data[0],
+                'text': noun_data[1],
+                'type': 'noun'
+            })
+        }
+        // add state nodes 
+        for (var stat_ind = 0; stat_ind < results[i][1].length; stat_ind++) {
+            var stat_data = results[i][1][stat_ind];
+            nodes_from_query.push({
+                'index': stat_data[0],
+                'text': stat_data[1],
+                'type': 'stat'
+            })
+        }
+        // add relation data
+        for (var rel = 0; rel < results[i][2].length; rel++) {
+            var rel_data = results[i][2][rel];
+            var relation = {
+                'source': id_from_url(rel_data[2].start),
+                'target': id_from_url(rel_data[2].end),
+                'type': rel_data[1]
+            };
+            // get properties of relation
+            var rel_properties = results[i][2][rel][2].data;
+            for (attr in rel_properties) {
+                relation[attr] = rel_properties[attr];
+            }
+            relations_for_viz.push(relation);
+        }
+    }
+
+    // console.log('nodes:', nodes_from_query);
+    // console.log('rels', relations_for_viz);
+    // console.log('nodes:', nodes_from_query[0]);
+    // console.log('rels', relations_for_viz[0]);
+
+
+    var nodes_for_viz = []
+
+    // transform graph ids to indices starting from 0 for vizualisation 
+    var node_ids = {}
+    var id_count = 0;
+    for (n in nodes_from_query) {
+        graph_id = nodes_from_query[n].index;
+
+        if (graph_id in node_ids) {
+            // console.log("seen", graph_id ," to ", node_ids[graph_id])
+            // nodes_from_query[n].index = node_ids[graph_id];
+        } else {
+            node_ids[graph_id] = id_count;
+            var new_node = nodes_from_query[n];
+            new_node.index = node_ids[graph_id];
+            nodes_for_viz.push(new_node);
+            id_count++;
+            // console.log("new", graph_id ," to ", node_ids[graph_id], new_node)
+        }
+    }
+    // change ids in relations from graph ids to new ones
+    for (rel in relations_for_viz) {
+        // console.log(relations_for_viz[rel], relations_for_viz[rel].source, " -- ", relations_for_viz[rel].target)
+        relations_for_viz[rel].source = node_ids[relations_for_viz[rel].source];
+        relations_for_viz[rel].target = node_ids[relations_for_viz[rel].target]
+
+    }
+
+    draw_graph(nodes_for_viz, relations_for_viz);
+}
