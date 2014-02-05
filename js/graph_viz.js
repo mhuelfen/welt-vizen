21bab7cab6c4e8e7946ea0b370438a90d19d7ad0
mhuelfen <huelfe@gmx.de>
2014-02-05 18:05:06 +0100

    stroke-width conditional on connection strength

--- js/graph_viz.js
+++ js/graph_viz.js
@@ -6,55 +6,64 @@ var paths;
 
 var mouseDown = 0;
 
+var max_stroke_width = 5;
+var min_stroke_width = 2;
+var stroke_range = max_stroke_width - min_stroke_width;
+var max_cos_dist = 0.88;
+var min_cos_dist = 0.65;
 
-function draw_legend(svg,legend_content,rel_types){
+var max_pmi = 0;
+var min_pmi = 1;
 
-	var legend = svg.append("g")
-	    .attr("class", "legend")
-	//.attr("x", w - 65)
-	//.attr("y", 50)
-	    .attr("height", 100)
-	    .attr("width", 100)
-	    .attr('transform', 'translate(-20,50)')
 
-	var w = svg[0][0].clientWidth;    
+function draw_legend(svg, legend_content, rel_types) {
 
-	legend.selectAll('rect')
-	    .data(rel_types)
-	    .enter()
-	    .append("rect")
-	    .attr("x", w - 165)
-	    .attr("y", function (d, i) {
-	        return i * 20 +5;
-	    })
-	    .attr("width", 20)
-	    .attr("height", 1)
-	    .style("stroke-width", 2)
-	    .style("stroke", function (d) {
-//	        var color = color_hash[dataset.indexOf(d)][1];
-		        var color = legend_content[d].color;
-		console.log(d);
-	        return color;
-	    });
-	
+    var legend = svg.append("g")
+        .attr("class", "legend")
+    //.attr("x", w - 65)
+    //.attr("y", 50)
+    .attr("height", 100)
+        .attr("width", 100)
+        .attr('transform', 'translate(-20,50)')
 
-	legend.selectAll('text')
-	    .data(rel_types)
-	    .enter()
-	    .append("text")
-	    .attr("x", w - 142)
-	    .attr("y", function (d, i) {
-	        return i * 20 + 9;
-	    })
-	    .text(function (d) {
-		    var text = legend_content[d].text;
-	    
-	    //    var text = color_hash[dataset.indexOf(d)][0];
-	        return text;
-	    });
+    var w = svg[0][0].clientWidth;
+
+    legend.selectAll('rect')
+        .data(rel_types)
+        .enter()
+        .append("rect")
+        .attr("x", w - 165)
+        .attr("y", function (d, i) {
+            return i * 20 + 5;
+        })
+        .attr("width", 20)
+        .attr("height", 1)
+        .style("stroke-width", 2)
+        .style("stroke", function (d) {
+            //          var color = color_hash[dataset.indexOf(d)][1];
+            var color = legend_content[d].color;
+            console.log(d);
+            return color;
+        });
+
+
+    legend.selectAll('text')
+        .data(rel_types)
+        .enter()
+        .append("text")
+        .attr("x", w - 142)
+        .attr("y", function (d, i) {
+            return i * 20 + 9;
+        })
+        .text(function (d) {
+            var text = legend_content[d].text;
+
+            //    var text = color_hash[dataset.indexOf(d)][0];
+            return text;
+        });
 
 }
-	
+
 function draw_graph(nodes_for_viz, relations_for_viz, paths_for_viz) {
     nodes = nodes_for_viz;
     rels = relations_for_viz;
@@ -63,8 +72,8 @@ function draw_graph(nodes_for_viz, relations_for_viz, paths_for_viz) {
 
     paths = paths_for_viz;
 
-    var width = 960,
-        height = 700;
+    var width = 600,
+        height = 680;
 
     var force = d3.layout.force()
         .nodes(nodes_for_viz)
@@ -79,64 +88,80 @@ function draw_graph(nodes_for_viz, relations_for_viz, paths_for_viz) {
     $('#graph_svg').remove();
 
     // make new graphic
-    var svg = d3.select("body").append("svg")
+//    var svg = d3.select("body").append("svg")
+    var svg = d3.select("#draw_area").append("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("id", 'graph_svg');
 
-    var rel_colors = {  "IN_STATE" : { color : "green", text : "noun in statement"},
-                        "MAY_BE" : { color :"violet", text : "noun may be"},
-                        "SIM_NOUN": { color :"brown",text : "similar nouns"},
-                        "SIM_STAT": { color :"blue",text : "similar statements"}
-                     }
-    var rel_types = ["IN_STATE","MAY_BE","SIM_NOUN","SIM_STAT"]
+    var rel_colors = {
+        "IN_STATE": {
+            color: "green",
+            text: "noun in statement"
+        },
+        "MAY_BE": {
+            color: "violet",
+            text: "noun may be"
+        },
+        "SIM_NOUN": {
+            color: "brown",
+            text: "similar nouns"
+        },
+        "SIM_STAT": {
+            color: "blue",
+            text: "similar statements"
+        }
+    }
+    var rel_types = ["IN_STATE", "MAY_BE", "SIM_NOUN", "SIM_STAT"]
 
     // arrow head
-	svg.append("defs").append("marker")
-	    .attr("id", "arrowhead")
-	    .attr("refX", node_diameter + 5) /*must be smarter way to calculate shift*/
-	    .attr("refY", 2)
-	    .attr("markerWidth", 6)
-	    .attr("markerHeight", 4)
-	    .attr("orient", "auto")
-	    .append("path")
-        .attr("d", "M 0,0 V 4 L6,2 Z");
+    svg.append("defs").append("marker")
+        .attr("id", "arrowhead")
+        .attr("viewBox", "0 0 10 10")
+        .attr("refX", node_diameter + 14) /*must be smarter way to calculate shift*/
+        .attr("refY", 5)
+        .attr("markerUnits", "strokeWidth")
+        .attr("markerWidth", 4)
+        .attr("markerHeight", 3)
+        .attr("orient", "auto")
+        .append("path")
+//        .style("stroke-opacity", 0.5)
+        .attr("d", "M 0 0 L 10 5 L 0 10 z");
 
     // graph legend
-    draw_legend(svg,rel_colors,rel_types);
+    draw_legend(svg, rel_colors, rel_types);
 
     var link = svg.selectAll(".link")
         .data(force.links())
         .enter().append("line")
         .attr("class", "link")
         .style("stroke", function (d) {
+            console.log("line", d.type)
             return rel_colors[d.type].color;
+
         })
-        .style("stroke-width", 1)
-        .attr("marker-end", function (d){
-             if (!(d.type == "SIM_NOUN" || d.type == "SIM_STAT"))
+        .style("stroke-width", function (d) {
+            if (d.type == "SIM_NOUN" || d.type == "SIM_STAT") {
+                // calc how much of stroke width range is used
+                var percent = (d.cos_dist - min_cos_dist) / (max_cos_dist - min_cos_dist);
+                console.log(d, d.cos_dist);
+                console.log("p", percent, "calc", d.cos_dist, min_cos_dist, "/", max_cos_dist, min_cos_dist);
+                console.log("w", min_stroke_width + stroke_range * percent);
+                return min_stroke_width + stroke_range * percent;
+            } else if (d.type == "IN_STATE") {
+                var percent = (d.pmi - min_pmi) / (max_pmi - min_pmi);
+                console.log(d,"pmi_stroke",min_stroke_width + stroke_range * percent, "p",percent)
+                return min_stroke_width + stroke_range * percent;
+
+            }
+        })
+        .attr("marker-end", function (d) {
+            if (!(d.type == "SIM_NOUN" || d.type == "SIM_STAT"))
                 return "url(#arrowhead)";
-        });
+        })
+        .style("stroke-opacity",0.5);
 
-    //         .style("stroke-width", function (d) {
-    //             if (d.type == "IN_STATE") {
-    //                 return 1;
-    //             } else {
-    //                 return 4;
-    //             }
-    //         })
-    // ;
-    // .on("mouseover", link_mouseover)
-    // .on("mouseout", link_mouseout);
 
-    link.append("text")
-        .attr("x", 12)
-        .attr("dy", ".35em")
-        .style("fill", "#000")
-        .text(function (d) {
-            return d.pmi;
-        });
-    // 
     var node = svg.selectAll(".node")
         .data(force.nodes())
         .enter().append("g")
@@ -237,7 +262,7 @@ function node_mouseover() {
 
 
         d3.selectAll(".link").style("stroke-opacity", function (l) {
-            return isInPaths(l.source.index, paths_with_node) && isInPaths(l.target.index, paths_with_node) ? 1 : 0.1;
+            return isInPaths(l.source.index, paths_with_node) && isInPaths(l.target.index, paths_with_node) ? 0.5 : 0.1;
         });
 
         //    //    highlight_node_links(d3.select(this),2);
@@ -267,7 +292,7 @@ function node_mouseout() {
                 return thisOpacity;
             });
         //show all links
-        d3.selectAll(".link").style("stroke-opacity", 1);
+        d3.selectAll(".link").style("stroke-opacity", 0.5);
 
         // d3.select(this).moveToBack();
 
@@ -304,58 +329,3 @@ $(document).mousedown(function () {
 $(document).mouseup(function () {
     --mouseDown;
 });
-// function link_mouseover() {
-//  console.log("over link")
-//     d3.select(this).select("line").transition()
-//         .style("stroke-width", 8);
-// }
-// 
-// function link_mouseout() {
-//     d3.select(this).select("line")//.transition()
-//         .style("stroke-width", 2);
-// }
-
-// function highlight_node_links(node,i){
-// 
-//   var remainingNodes=[],
-//       nextNodes=[];
-// 
-//   var stroke_opacity = 0;
-//   if( d3.select(this).attr("data-clicked") == "1" ){
-//     d3.select(this).attr("data-clicked","0");
-//     stroke_opacity = 0.2;
-//   }else{
-//     d3.select(this).attr("data-clicked","1");
-//     stroke_opacity = 0.5;
-//   }
-// 
-//   var traverse = [{
-//                     linkType : "sourceLinks",
-//                     nodeType : "target"
-//                   },{
-//                     linkType : "targetLinks",
-//                     nodeType : "source"
-//                   }];
-// 
-//   traverse.forEach(function(step){
-//     node[step.linkType].forEach(function(link) {
-//       remainingNodes.push(link[step.nodeType]);
-//       highlight_link(link.id, stroke_opacity);
-//     });
-// 
-//     while (remainingNodes.length) {
-//       nextNodes = [];
-//       remainingNodes.forEach(function(node) {
-//         node[step.linkType].forEach(function(link) {
-//           nextNodes.push(link[step.nodeType]);
-//           highlight_link(link.id, stroke_opacity);
-//         });
-//       });
-//       remainingNodes = nextNodes;
-//     }
-//   });
-// }
-// 
-// function highlight_link(id,opacity){
-//     d3.select("#link-"+id).style("stroke-opacity", opacity);
-// }
